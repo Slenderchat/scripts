2a3501d63d614844d83e9e8c2ec93886f7db1063
Slenderchat <steeplike@gmail.com>
2019-08-08 15:52:36 +0300

    Added logging system to cleanify script's output

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ chkBld () {
 		let ER++;
 	else
 		echo "Building failed";
-		make -j 1 V=s
+		make -j 1 V=s 2>> build-errors.log 1>> build-output.log
 		if [  $? -eq 0  ]
 		then
 			echo "Second building attempt succesful";
@@ -36,19 +36,28 @@ clean () {
 	rm -rf files/*
 	chk "Cleaning files"
 }
+cleanLogs () {
+	rm -f build.log
+	chk "Removing build.log"
+	rm -f build-output.log
+	chk "Removing build-output.log"
+	rm -f build-errors.log
+	chk "Removing build-errors.log"
+}
 build () {
+	echo "Building $1"
 	clean
 	cp -f ../openwrt-config/$1/.config .
 	chk "Copying .config"
 	cp -rf ../openwrt-config/$1/etc files
 	chk "Copying files"
-	make defconfig
+	make defconfig 2>> build-errors.log 1>> build-output.log
 	chk "Expanding .config"
-	make clean
+	make clean 2>> build-errors.log 1>> build-output.log
 	chk "Cleaning"
-	make download
+	make download 2>> build-errors.log 1>> build-output.log
 	chk "Downloading"
-	make -j 1 V=s
+	make -j 1 V=s 2>> build-errors.log 1>> build-output.log
 	chkBld
 	for file in bin/targets/*/*/openwrt-*-squashfs-*.bin
 	do
@@ -57,10 +66,12 @@ build () {
 		mv $file "${file%%openwrt-*-squashfs-*.bin}${1}-${file2}"
 	done
 	chk "Copying results"
-	make clean
+	make clean 2>> build-errors.log 1>> build-output.log
 	chk "Cleaning"
 	clean
+	echo "\n"
 }
+cleanLogs
 build "sverdlova-1"
 build "sverdlova-2"
 build "danilevskii-1"
