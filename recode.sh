a922778202d656f5961798e4efd0528a87c31e71
Slenderchat <steeplike@gmail.com>
2019-08-26 05:22:52 +0300

    recode.sh: Changed crf to 18 for nearly lossless video, added mp4 faststart, added timescale for concatenated file

--- recode.sh
+++ recode.sh
@@ -3,13 +3,13 @@ TF=$(mktemp)
 TD=$(mktemp -d)
 for file in [0-9][0-9]-[0-9][0-9]-[0-9][0-9].mp4
 do
-	ffmpeg -i $file -an -c:v libx264 -pix_fmt yuv420p -s 1280x720 -profile:v baseline -level 31 -r 10 -video_track_timescale 10 -y "$TD/$(basename $file)"
+	ffmpeg -i $file -an -c:v libx264 -pix_fmt yuv420p -s 1280x720 -profile:v baseline -level 31 -crf 18 -r 10 -movflags +faststart -video_track_timescale 10 -y "$TD/$(basename $file)"
 done
 for file in $TD/*.mp4
 do
 	echo "file '$file'" >> $TF
 done
-ffmpeg -f concat -safe 0 -i $TF -c:v copy -an -y out.mp4
+ffmpeg -f concat -safe 0 -i $TF -c:v copy -movflags +faststart -video_track_timescale 10 -an -y out.mp4
 rm -f $TF
 rm -rf $TD
 
