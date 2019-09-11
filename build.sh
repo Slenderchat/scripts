9f480a172082e611bc236705c3d18a01f788f956
Slenderchat <steeplike@gmail.com>
2019-09-11 10:15:53 +0300

    Slight bugfixes and improvements

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ build () {
 	echo "Pre-build cleaning" &&
 	rm -rf .config .config.old files/* bin/* build_dir/* tmp/*
 	echo "Copying configuration" &&
-	cp -f $1/config .config &&
+	cp -f ../openwrt-config/$1/config .config &&
 	echo "Copying files" &&
-	cp -Lrf $1/etc files &&
+	cp -Lrf ../openwrt-config/$1/etc files &&
 	echo "Expanding configuration" &&
 	make defconfig >> build.log 2>&1 &&
 	echo "Downloading" &&
@@ -24,32 +24,30 @@ build () {
 		fi
 	fi
 	echo "Copying results" &&
-	for file in bin/targets/*/*/openwrt-*-squashfs-*.bin
+	for firmware in bin/targets/**/**/*.bin
 	do
-		local tmp=$(basename $file) &&
-		tmp=${file2#*-*-*-*-*-*-*-} &&
-		mv $file ../openwrt-firmware/$(basename ${file%openwrt-*-squashfs-*.bin}$1-$tmp)
+		tmp=$(basename $firmware) &&
+		tmp=$(echo -n $tmp | perl -ne 's/.*?-(?=tftp-|sysupgrade|factory)//g; print;') &&
+		tmp=$1-$tmp &&
+		mv $firmware ../openwrt-firmware/$tmp
 	done
 	echo
 }
-echo "Removing build log" &&
-rm -f build.log &&
 echo "Fetching from GIT" &&
 git fetch >> build.log 2>&1 &&
 echo "Reseting working tree to origin/master's state" &&
 git reset --hard origin/master >> build.log 2>&1 &&
+echo "Cleaning working tree" &&
+git clean -ffdx -e build.sh -e staging_dir >> build.log 2>&1 &&
 echo "Updating feeds" &&
 scripts/feeds update -a >> build.log 2>&1 &&
 echo "Installing feeds" &&
 scripts/feeds install -a >> build.log 2>&1 &&
-echo "Creating 'build_dir'" &&
-mkdir -p /tmp/build_dir &&
-#echo "Creating symlink to 'build_dir'" &&
+#echo "Creating 'build_dir'" &&
+#mkdir -p /tmp/build_dir &&
 #ln -sf /tmp/build_dir build_dir &&
-echo "Creating 'files'" &&
+echo "Creating directories" &&
 mkdir -p files &&
-echo "Cleaning and creating 'openwrt-firmware'" &&
-rm -rf ../openwrt-firmware &&
 mkdir -p ../openwrt-firmware &&
 echo
 if [ $# -gt 0 ]
@@ -59,11 +57,11 @@ then
 		build $arg
 	done
 else
-	for tgt in *-[1-9]
+	for tgt in ../openwrt-config/*-[1-4]
 	do
 		build $(basename $tgt)
 	done
 fi
-echo "Nuking junk files and folders"
-rm -rf bin build_dir files tmp
+echo "Cleaning working tree"
+git clean -ffdx -e build.sh -e staging_dir >> build.log 2>&1
 
