5e911438aaef340774ced2608fc59b5b14cc9e95
slenderchat <steeplike@gmail.com>
2019-09-28 01:31:00 +0300

    Optimized 'scanWifiChannel.sh'

--- scanWifiChannel.sh
+++ scanWifiChannel.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-echo "Sverdlova-1:"
-ssh root@192.168.0.1 "iw dev ap scan | grep \"primary channel\" | grep \"\b1\b\|\b6\b\|\b11\b\" | sort -V | uniq -c"
-echo "Sverdlova-2:"
-ssh root@192.168.0.2 "iw dev ap scan | grep \"primary channel\" | grep \"\b1\b\|\b6\b\|\b11\b\" | sort -V | uniq -c"
-echo "Danilevskii-1:"
-ssh root@192.168.1.1 "iw dev ap scan | grep \"primary channel\" | grep \"\b1\b\|\b6\b\|\b11\b\" | sort -V | uniq -c"
-echo "Danilevskii-2:"
-ssh root@192.168.1.2 "iw dev ap scan | grep \"primary channel\" | grep \"\b1\b\|\b6\b\|\b11\b\" | sort -V | uniq -c"
-echo "Danilevskii-3:"
-ssh root@192.168.1.3 "iw dev ap scan | grep \"primary channel\" | grep \"\b1\b\|\b6\b\|\b11\b\" | sort -V | uniq -c"
-echo "Danilevskii-4:"
-ssh root@192.168.1.4 "iw dev ap scan | grep \"primary channel\" | grep \"\b1\b\|\b6\b\|\b11\b\" | sort -V | uniq -c"
-echo "Spb-1:"
-ssh root@192.168.2.1 "iw dev wlan1 scan | grep \"primary channel\" | grep \"\b1\b\|\b6\b\|\b11\b\" | sort -V | uniq -c"
+CMD="iw dev ap_2G scan | grep \"primary channel\" | grep \"\b1\b\|\b6\b\|\b11\b\" | sort -V | uniq -c | sort -V"
+CHA="iw dev ap_2G info | grep \"channel\""
+getInfo() {
+	echo "${1}: $(ssh root@${2} ${CHA} | sed 's/channel \(.*\) (.*/\1/' | tr -d '\t')"
+	ssh root@${2} ${CMD} | tr -d '\t'
+}
+getInfo Sverdlova-1 192.168.0.1
+getInfo Sverdlova-2 192.168.0.2
+getInfo Danilevskii-1 192.168.1.1
+getInfo Danilevskii-2 192.168.1.2
+getInfo Danilevskii-3 192.168.1.3
+getInfo Danilevskii-4 192.168.1.4
+getInfo Spb-1 192.168.2.1
 
